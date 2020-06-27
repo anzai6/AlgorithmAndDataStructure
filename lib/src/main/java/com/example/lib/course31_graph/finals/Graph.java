4b109eeb5fae4faf56ba0a6b13414221187337a2
anzai6 <csu_anzai@126.com>
2020-06-28 00:09:41 +0800

    课程三十一：有向图复习

--- lib/src/main/java/com/example/lib/course31_graph/finals/Graph.java
+++ lib/src/main/java/com/example/lib/course31_graph/finals/Graph.java
@@ -1,5 +1,6 @@
 package com.example.lib.course31_graph.finals;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -15,6 +16,14 @@ public class Graph {
     private LinkedList<Integer>[] adj; // 顶点的邻接表
 
     public Graph(int v) {
+        if (v <= 0) {
+            throw new IllegalArgumentException("v is invalid");
+        }
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
     }
 
     /**
@@ -24,6 +33,11 @@ public class Graph {
      * @param t
      */
     public void addEdge(int s, int t) {
+        if (s >= v || t >= v || s == t) {
+            return;
+        }
+        adj[s].add(t);
+        adj[t].add(s);
     }
 
 
@@ -34,6 +48,34 @@ public class Graph {
      * @param t
      */
     public void bfs(int s, int t) {
+        // 广度优先即按层遍历
+        boolean[] visited = new boolean[v]; // 存储已经遍历的点
+        int[] prev = new int[v]; // 记录每一个被访问到的节点的路径上的前一个节点
+        for (int i = 0; i < v; i++) {
+            prev[i] = -1;
+        }
+        LinkedList<Integer> list = new LinkedList<>(); // 存放正在遍历的点
+        list.add(s);
+        visited[s] = true;
+        while (!list.isEmpty()) {
+            int i = list.poll();
+            if (i == t) {
+                printRoute(prev, t);
+                break;
+            }
+            LinkedList<Integer> edgeList = adj[i];
+            Iterator<Integer> iterator = edgeList.iterator();
+            while (iterator.hasNext()) {
+                int h = iterator.next();
+                // 没有访问过就添加
+                if (!visited[h]) {
+                    list.add(h);
+                    visited[h] = true;
+                    // 设置前置路径，方便打印
+                    prev[h] = i;
+                }
+            }
+        }
     }
 
     /**
@@ -43,11 +85,45 @@ public class Graph {
      * @param t
      */
     public void dfs(int s, int t) {
+        boolean[] visited = new boolean[v]; // 存储已经遍历的点
+        int[] prev = new int[v]; // 记录每一个被访问到的节点的路径上的前一个节点
+        for (int i = 0; i < v; i++) {
+            prev[i] = -1;
+        }
+        visited[s] = true;
+        re_dfs(s, t, visited, prev);
     }
 
     boolean foundRoute = false;
 
     private void re_dfs(int s, int t, boolean[] visited, int[] prev) {
+        if (s == t) {
+            foundRoute = true;
+            printRoute(prev, t);
+            return;
+        }
+        if (foundRoute) {
+            return;
+        }
+        LinkedList<Integer> list = adj[s];
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            int h = iterator.next();
+            // 没有访问过
+            if (!visited[h]) {
+                visited[h] = true;
+                // 设置前置路径，方便打印
+                prev[h] = s;
+                // 继续深入
+                re_dfs(h, t, visited, prev);
+                if (foundRoute) {
+                    return;
+                }
+                // 不行的话，回溯
+                visited[h] = false;
+                prev[h] = -1;
+            }
+        }
 
     }
 
@@ -58,7 +134,12 @@ public class Graph {
      * @param t
      */
     private void printRoute(int[] prev, int t) {
-
+        if (prev[t] >= 0) {
+            printRoute(prev, prev[t]);
+            System.out.print(" -> " + t);
+        } else {
+            System.out.print(t + "");
+        }
     }
 
     /**
@@ -113,11 +194,11 @@ public class Graph {
         // 3 —— 4 —— 5
         //      |    |
         //      6 —— 7
-//        myGraph.bfs(0,7); // 0-3-4-6-7
-//        myGraph.dfs(0, 7);
+//        myGraph.bfs(0, 7); // 0-3-4-6-7
+        myGraph.dfs(0, 7);
 
         // 获取n度好友
-        myGraph.getThreeByBfs(0, 1);
+//        myGraph.getThreeByBfs(0, 1);
 //        myGraph.getThreeByDfs(0, 2); // 实现有问题，不能准确
     }
 
