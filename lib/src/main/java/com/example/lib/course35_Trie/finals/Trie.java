05039addac8475fa517c2d3f36bb2a246d7e40a9
qinshunan <qinshunan@bbk.com>
2020-09-19 11:34:09 +0800

    课程三十五：Trie 树复习

--- lib/src/main/java/com/example/lib/course35_Trie/finals/Trie.java
+++ lib/src/main/java/com/example/lib/course35_Trie/finals/Trie.java
@@ -5,27 +5,55 @@ package com.example.lib.course35_Trie.finals;
  * 还有输入法的自动补全，编辑器的自动补全等等，不太适合完全匹配的字符串查找，耗费内存过多，这个用红黑树或者散列表更合适
  * Created by qinshunan on 2019/4/19.
  */
-
 public class Trie {
     private TrieNode headNode = new TrieNode('/'); // 存储无意义字符
 
     /**
-     * 往 ACAutomata 树中插入一个字符串
+     * 往 Trie 树中插入一个字符串
      *
      * @param list
      * @param len
      */
     public void insert(char[] list, int len) {
+        if (list == null || list.length == 0) {
+            System.out.println("list is empty");
+            return;
+        }
+        TrieNode trieNode = headNode;
+        for (int i = 0; i < len; i++) {
+            int index = list[i] - 'a';
+            TrieNode subTrieNode = trieNode.subTrieList[index];
+            if (subTrieNode == null) {
+                subTrieNode = new TrieNode(list[i]);
+                trieNode.subTrieList[index] = subTrieNode;
+            }
+            trieNode = subTrieNode;
+        }
+        trieNode.isEndingChar = true;
     }
 
     /**
-     * 在 ACAutomata 树中查找一个字符串
+     * 在 Trie 树中查找一个字符串
      *
      * @param list
      * @param len
      */
     public boolean find(char[] list, int len) {
-        return false;
+        if (list == null || list.length == 0) {
+            System.out.println("list is empty");
+            return false;
+        }
+        TrieNode trieNode = headNode;
+        for (int i = 0; i < len; i++) {
+            int index = list[i] - 'a';
+            TrieNode subTrieNode = trieNode.subTrieList[index];
+            if (subTrieNode == null) {
+                return false;
+            }
+            trieNode = subTrieNode;
+        }
+        // 不能完全匹配，只是前缀则会返回false
+        return trieNode.isEndingChar;
     }
 
 
